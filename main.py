52ee515d27ba16223bbd4056740490654f04885a
BlackSpy369 <blackspyyt369@gmail.com>
2023-11-12 18:58:17 +0530

    Added face detection.

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ CLASSES=["Average","Beautiful"]
 
 cap=cv2.VideoCapture(0)
 
+haar_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 while True:
     ret,frame=cap.read()
     if not ret:
         print("Cannot Open Camera !")
 
-    img=cv2.resize(frame,(256,256))
-    # cv2.imshow("Img",img)
-    img_batch=np.expand_dims(img,axis=0)
-    y_pred=MODEL.predict(img_batch)
-    belong_class=CLASSES[round(y_pred[0][0])]
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    frame=cv2.putText(frame,belong_class,(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2,cv2.LINE_AA)
+    for x,y,w,h in haar_cascade.detectMultiScale(gray_frame):
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2,cv2.LINE_AA)
+
+        img=frame[y:y+h,x:x+w]
+        img=cv2.resize(img,(256,256))
+        # cv2.imshow("Img_",img)
+
+        img_batch=np.expand_dims(img,axis=0)
+        y_pred=MODEL.predict(img_batch)
+        belong_class=CLASSES[round(y_pred[0][0])]
+
+        frame=cv2.putText(frame,belong_class,(x,w),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2,cv2.LINE_AA)
+
     cv2.imshow("Frame",frame)
 
     if cv2.waitKey(1)==ord("q"):
